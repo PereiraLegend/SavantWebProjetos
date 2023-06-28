4949ae4fcdd8618890a67ffa4e856ba19624240e
PereiraLegend <pereirapro123@gmail.com>
2023-06-28 12:01:23 -0300

    commit

--- ProjetoClinica/butoes.js
+++ ProjetoClinica/butoes.js
@@ -91,3 +91,12 @@ function ocultar(){
   }
 }
 ocultar()
+
+/**************************************************************************************** */
+/**Efeito de revelar os itens na página*/
+window.sr = ScrollReveal({ reset:true })
+sr.reveal('.area1', {duration: 2000}),
+sr.reveal('.area2', {duration: 2000}),
+sr.reveal('.area3', {duration: 2000}),
+sr.reveal('.area4', {duration: 2000}),
+sr.reveal('.area5', {duration: 2000})
